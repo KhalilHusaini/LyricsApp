6b56779e544aa5a7e60189353b330ee71a5d1834
khaikyler <132238349+khaikyler@users.noreply.github.com>
2023-06-14 16:07:04 +0800

    Update ChosenSongPage.jsx

--- project2/src/Pages/ChosenSongPage.jsx
+++ project2/src/Pages/ChosenSongPage.jsx
@@ -6,35 +6,45 @@ import './ChosenSongPage.css';
 export default function ChosenSongPage() {
   const { id } = useParams();
   const [lyrics, setLyrics] = useState('');
+  const [songDetails, setSongDetails] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchLyrics = async () => {
+    const fetchSongDetailsAndLyrics = async () => {
       try {
-        const response = await fetch(
+        const lyricsResponse = await fetch(
           `http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${apiKey}`
         );
-        const data = await response.json();
+        const lyricsData = await lyricsResponse.json();
 
-        if (response.ok) {
-          setLyrics(data.message.body.lyrics.lyrics_body);
+        if (lyricsResponse.ok) {
+          setLyrics(lyricsData.message.body.lyrics.lyrics_body);
         } else {
-          console.log('Error:', data.message.body);
+          console.log('Error:', lyricsData.message.body);
+        }
+
+        const detailsResponse = await fetch(
+          `http://api.musixmatch.com/ws/1.1/track.get?track_id=${id}&apikey=${apiKey}`
+        );
+        const detailsData = await detailsResponse.json();
+
+        if (detailsResponse.ok) {
+          setSongDetails(detailsData.message.body.track);
+        } else {
+          console.log('Error:', detailsData.message.body);
         }
       } catch (error) {
         console.log('Error:', error);
       }
     };
 
-    fetchLyrics();
+    fetchSongDetailsAndLyrics();
   }, [id]);
 
   const handleBackToSearchResults = () => {
     const searchQuery = new URLSearchParams(location.search).get('query');
-    navigate(searchQuery || '/'); 
+    navigate(searchQuery || '/');
   };
-  
-  
 
   const handleBackToHomepage = () => {
     navigate('/');
@@ -50,7 +60,22 @@ export default function ChosenSongPage() {
           Back to Homepage
         </button>
         <h1>Chosen Song Page</h1>
-        <div>{lyrics}</div>
+        {lyrics ? (
+          <div>
+            <div>{lyrics}</div>
+            {songDetails && (
+              <div className="song-details">
+                <h2>{songDetails.track_name}</h2>
+                <h3>{songDetails.artist_name}</h3>
+                <p>{songDetails.album_name}</p>
+                <p>{songDetails.track_rating}</p>
+                <p>{songDetails.track_length}</p>
+              </div>
+            )}
+          </div>
+        ) : (
+          <p>Loading lyrics...</p>
+        )}
       </div>
     </div>
   );
